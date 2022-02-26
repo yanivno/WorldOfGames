5aff88dd1dd224bdf34ca10617f169cebc540c84
ynorman <yaniv.no@gmail.com>
2022-02-26 11:01:21 +0200

    e2e modifications for cli run

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,30 +1,43 @@
-from utils import FLASK_IP_BIND, FLASK_PORT_BIND
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import sys
 import os
 import logging
-from utils import GOOD_RETURN_CODE,BAD_RETURN_CODE
+import sys
+import traceback
+
+# importing utils from file system path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+sys.path.append(parent_dir)
+from utils import FLASK_IP_BIND, FLASK_PORT_BIND
+from utils import GOOD_RETURN_CODE, BAD_RETURN_CODE
 
 
-def test_scores_service(URL):
-    drv_file = os.path.join(os.path.dirname(__file__), 'chromedriver')
-    driver = webdriver.Chrome(executable_path=drv_file)
-    driver.get(URL)
-    scores = driver.find_elements(By.XPATH, "//*[contains(@id, 'score_')]")
+def test_scores_service(uri):
     return_code = GOOD_RETURN_CODE
-    for score in scores:
-        if not (score.text.isnumeric() and 0 <= int(score.text) <= 100):
-            return_code = BAD_RETURN_CODE
-    driver.close()
+    try:
+        drv_file = os.path.join(os.path.dirname(__file__), 'chromedriver')
+        driver = webdriver.Chrome(executable_path=drv_file)
+        driver.get(uri)
+        scores = driver.find_elements(By.XPATH, "//*[contains(@id, 'score_')]")
+        for score in scores:
+            if not (score.text.isnumeric() and 0 <= int(score.text) <= 100):
+                return_code = BAD_RETURN_CODE
+    except:
+        traceback.print_exc()
+        return_code = BAD_RETURN_CODE
+    finally:
+        driver.close()
     return return_code
 
 
-def main_function():
-    uri = f"http://{FLASK_IP_BIND}:{FLASK_PORT_BIND}"
-    exit_code = test_scores_service(uri)
+def main_function(test_uri):
+    exit_code = test_scores_service(test_uri)
     logging.log(0, f"scoring module testing exit code {exit_code}")
     assert exit_code == GOOD_RETURN_CODE
 
 
-main_function()
+uri = f"http://{FLASK_IP_BIND}:{FLASK_PORT_BIND}"
+if len(sys.argv) > 1 and sys.argv[1]:
+    uri = sys.argv[1]
+print(f"testing address {uri}")
+main_function(uri)
