77c974130bdd1c6b992e553d0e4a5e54405234f3
ynorman <yaniv.no@gmail.com>
2022-02-26 09:41:54 +0200

    update test with const values and e2e test with assert

--- tests/e2e.py
+++ tests/e2e.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 import sys
 import os
 import logging
+from utils import GOOD_RETURN_CODE,BAD_RETURN_CODE
 
 
 def test_scores_service(URL):
@@ -11,21 +12,19 @@ def test_scores_service(URL):
     driver = webdriver.Chrome(executable_path=drv_file)
     driver.get(URL)
     scores = driver.find_elements(By.XPATH, "//*[contains(@id, 'score_')]")
-    is_valid = True
+    return_code = GOOD_RETURN_CODE
     for score in scores:
         if not (score.text.isnumeric() and 0 <= int(score.text) <= 100):
-            is_valid = False
+            return_code = BAD_RETURN_CODE
     driver.close()
-    return is_valid
+    return return_code
 
 
 def main_function():
     uri = f"http://{FLASK_IP_BIND}:{FLASK_PORT_BIND}"
-    score = test_scores_service(uri)
-    exit_code = -1
-    if score:
-        exit_code = 0
+    exit_code = test_scores_service(uri)
     logging.log(0, f"scoring module testing exit code {exit_code}")
+    assert exit_code == GOOD_RETURN_CODE
 
 
 main_function()
